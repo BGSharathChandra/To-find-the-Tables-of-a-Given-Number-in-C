5ee832102c9e68b5766496353041e2c0df1cb2df
BGSharathChandra <82028331+BGSharathChandra@users.noreply.github.com>
2021-04-08 21:37:03 +0530

    Add files via upload

---
+++ To_Find_The_Tables_of_a_Given_Number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    float num;
+
+printf("Enter the Number:");
+scanf("%f",&num);
+
+printf("The Tables of the %f\n\n",num);
+
+printf("%f * 1 = %f \n",num,num*1);
+printf("%f * 2 = %f \n",num,num*2);
+printf("%f * 3 = %f \n",num,num*3);
+printf("%f * 4 = %f \n",num,num*4);
+printf("%f * 5 = %f \n",num,num*5);
+printf("%f * 6 = %f \n",num,num*6);
+printf("%f * 7 = %f \n",num,num*7);
+printf("%f * 8 = %f \n",num,num*8);
+printf("%f * 9 = %f \n",num,num*9);
+printf("%f * 10 = %f \n",num,num*10);
+return 0;
+}
+
+
+
+
